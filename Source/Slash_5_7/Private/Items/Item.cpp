196d4cc2647b5a63e7888903f4d61b7776d3652a
acliang1412 <acliang7229@gmail.com>
2026-08-27 23:48:48 -0500

    Set Minimal_Default as startup map, add debug sphere & logging
    Changed default/editor maps to Minimal_Default in DefaultEngine.ini.
    Updated BP_Item.uasset and Minimal_Default.umap assets.
    Added DRAW_SPHERE macro and debug sphere drawing in AItem::BeginPlay().
    Added DeltaTime and item name logging in AItem::Tick().
    Included DrawDebugHelpers and project header in Item.cpp.

--- Source/Slash_5_7/Private/Items/Item.cpp
+++ Source/Slash_5_7/Private/Items/Item.cpp
@@ -2,6 +2,10 @@
 
 
 #include "Items/Item.h"
+#include "DrawDebugHelpers.h"
+#include "Slash_5_7/Slash_5_7.h"
+#define THIRTY 30 
+
 
 // Sets default values
 AItem::AItem()
@@ -22,6 +26,13 @@ void AItem::BeginPlay()
 	{
 		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Cyan, FString("Item OnScreen Message!"));
 	}
+
+	UWorld* World = GetWorld();
+	
+	FVector Location = GetActorLocation();
+	DRAW_SPHERE(Location)
+
+	
 	
 }
 
@@ -30,5 +41,17 @@ void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	UE_LOG(LogTemp, Warning, TEXT("Delta Time: %f"), DeltaTime);
+
+	if (GEngine)
+	{
+		FString Name = GetName();
+		FString Message = FString::Printf(TEXT("ItemName: %s"), *Name);
+		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Cyan, Message);
+
+		UE_LOG(LogTemp, Warning, TEXT("ItemName: %s"), *Name);
+	}
+	
 }
+
 

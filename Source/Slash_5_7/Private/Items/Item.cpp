6d628c8afe83a58c39855a061e1bdcf719145f6f
acliang1412 <acliang7229@gmail.com>
2026-09-21 20:30:29 -0500

    Refactor AItem: new ItemMesh root, sine params, cleanup
    Added ItemMesh as root component for AItem. Introduced Amplitude and TimeConstant properties for Blueprint editing, and private RunningTime for elapsed time tracking. Added BlueprintPure TransformedSin/TransformedCosin functions and a templated Avg() utility. Removed Tick logic for movement and debug drawing. Updated asset references to reflect component and property changes.

--- Source/Slash_5_7/Private/Items/Item.cpp
+++ Source/Slash_5_7/Private/Items/Item.cpp
@@ -13,7 +13,8 @@ AItem::AItem()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-
+	ItemMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ItemMeshComponent"));
+	RootComponent = ItemMesh;
 }
 
 // Called when the game starts or when spawned
@@ -24,20 +25,23 @@ void AItem::BeginPlay()
 	
 }
 
+float AItem::TransformedCosin()
+{
+	return Amplitude * FMath::Cos(RunningTime * TimeConstant);;
+}
+
+float AItem::TransformedSin()
+{
+	return Amplitude * FMath::Sin(RunningTime * TimeConstant);
+}
+
 // Called every frame
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 	RunningTime += DeltaTime;
 
-	float DeltaZ = Amplitude * FMath::Sin(RunningTime * TimeConstant);
 
-	AddActorWorldOffset(FVector(0.f, 0.f, DeltaZ));
-
-	
-	DRAW_SPHERE_SINGLEFRAME(GetActorLocation());
-	DRAW_VECTOR_SingleFrame(GetActorLocation(), GetActorLocation() + GetActorForwardVector() * 100.f);
 	
 }
 
